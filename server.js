f388fa6e32966af0883bdae50c49f69e768cb091
mrafcent1 <alexfcenteno@outlook.com>
2021-03-22 15:55:43 -0400

    login/logout

--- server.js
+++ server.js
@@ -12,6 +12,24 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 
+//session
+const session = require('express-session');
+
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+const sess = {
+  secret: 'Super secret secret',
+  cookie: {},
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize
+  })
+};
+
+app.use(session(sess));
+
+
 //handlebars
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
